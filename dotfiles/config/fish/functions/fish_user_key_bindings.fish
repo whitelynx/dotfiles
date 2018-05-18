74c57da2c4a6f3c9bc73f2fe4bb2aa3dde4ffdc3
David H. Bronke <whitelynx@gmail.com>
2018-05-18 12:25:49 -0400

    Updated fish keybindings to prioritize leonerd-style key sequences

--- dotfiles/config/fish/functions/fish_user_key_bindings.fish
+++ dotfiles/config/fish/functions/fish_user_key_bindings.fish
@@ -1,15 +1,19 @@
 function fish_user_key_bindings
 	fish_default_key_bindings
 
+	# Key sequences based on http://www.leonerd.org.uk/hacks/fixterms
+	bind \u009b'127;5u' backward-kill-word  # Ctrl+Backspace: delete 1 word to the left
+	bind \e'[127;5u' backward-kill-word     # Ctrl+Backspace: delete 1 word to the left
+	bind \u009b'1;5D' backward-word         # Ctrl+Left: move 1 word to the left
+	bind \e'[1;5D' backward-word            # Ctrl+Left: move 1 word to the left
+	bind \u009b'1;5C' forward-word          # Ctrl+Right: move 1 word to the right
+	bind \e'[1;5C' forward-word             # Ctrl+Right: move 1 word to the right
+	bind \u009b'3;5~' kill-word             # Ctrl+Delete: delete 1 word to the right
+	bind \e'[3;5~' kill-word                # Ctrl+Delete: delete 1 word to the right
+
+	# Backwards-compatibility for urxvt
 	bind \eOd backward-word       # Ctrl+Left: move 1 word to the left
 	bind \eOc forward-word        # Ctrl+Right: move 1 word to the right
 	bind \e'[3^' kill-word        # Ctrl+Delete: delete 1 word to the right
 	bind \ch backward-kill-word   # Ctrl+Backspace: delete 1 word to the left
-
-	# Different keycodes produced inside tmux (byobu):
-	bind \e'[1;5D' backward-word  # Ctrl+Left: move 1 word to the left
-	bind \e'[1;5C' forward-word   # Ctrl+Right: move 1 word to the right
-	bind \e'[3;5~' kill-word      # Ctrl+Delete: delete 1 word to the right
 end
-
-fzf_key_bindings
