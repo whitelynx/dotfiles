75ecf409751fa29a9c1ff84fbde17e3d0c1a0873
David H. Bronke <whitelynx@gmail.com>
2020-08-03 11:57:21 -0400

    prefer firefox over other browsers

--- dotfiles/config/fish/conf.d/vars.fish
+++ dotfiles/config/fish/conf.d/vars.fish
@@ -1,7 +1,7 @@
 #set -xg EDITOR (which gvim)
 set -xg EDITOR (which nvim)
 
-for browser in brave chrome chrome-browser chromium chromium-browser firefox
+for browser in firefox brave chrome chrome-browser chromium chromium-browser
 	if command -s $browser >/dev/null
 		set -xg BROWSER (which $browser)
 		break
