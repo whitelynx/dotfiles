d71369659640999fd589a7fa0eacb19971cda529
David H. Bronke <whitelynx@gmail.com>
2022-01-10 15:58:45 +0100

    re-bootstrapped

--- dotdrop.sh
+++ dotdrop.sh
@@ -10,7 +10,7 @@ if ! ${rl} "${0}" >/dev/null 2>&1; then
   rl="realpath"
 
   if ! hash ${rl}; then
-    echo "\"${rl}\" not found !" && exit 1
+    echo "\"${rl}\" not found!" && exit 1
   fi
 fi
 
@@ -22,10 +22,25 @@ cfg="${cur}/config.yaml"
 sub="dotdrop"
 
 # pivot
-cd "${cur}" || { echo "Directory \"${cur}\" doesn't exist, aborting." && exit; }
+cd "${cur}" || { echo "Directory \"${cur}\" doesn't exist, aborting." && exit 1; }
+
 # init/update the submodule
-git submodule update --init --recursive
+if [ "${DOTDROP_AUTOUPDATE-yes}" = yes ] ; then
+  git submodule update --init --recursive
+  git submodule update --remote dotdrop
+fi
+
+# check python executable
+pybin="python3"
+hash ${pybin} 2>/dev/null || pybin="python"
+[[ "`${pybin} -V 2>&1`" =~ "Python 3" ]] || { echo "install Python 3" && exit 1; }
+
 # launch dotdrop
-PYTHONPATH=dotdrop python3 -m dotdrop.dotdrop --cfg="${cfg}" "${args[@]}"
+PYTHONPATH=dotdrop:${PYTHONPATH} ${pybin} -m dotdrop.dotdrop "${args[@]}"
+ret="$?"
+
 # pivot back
-cd "${opwd}" || { echo "Directory \"${opwd}\" doesn't exist, aborting." && exit; }
+cd "${opwd}" || { echo "Directory \"${opwd}\" doesn't exist, aborting." && exit 1; }
+
+# exit with dotdrop exit code
+exit ${ret}
