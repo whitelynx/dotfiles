f80645e277a26bc4707d067c025132ed5775665f
David H. Bronke <whitelynx@gmail.com>
2022-06-01 04:10:11 +0200

    Wayland: consolidate env var config

--- dotfiles/config/fish/functions/startsway.fish
+++ dotfiles/config/fish/functions/startsway.fish
@@ -1,14 +1,8 @@
 # Defined in /tmp/fish.RG4gxf/startsway.fish @ line 2
 function startsway --description 'Start the Sway Wayland compositor'
-	set -x XCURSOR_THEME xcursor-breeze
-	set -x XCURSOR_SIZE 48
-	set -x QT_SCALE_FACTOR 1
-	set -x QT_QPA_PLATFORM wayland-egl
-	set -x CLUTTER_BACKEND wayland
-	set -x BEMENU_BACKEND wayland
-	set -x MOZ_ENABLE_WAYLAND 1
-	set -x SDL_VIDEODRIVER wayland
+	export (cat ~/.config/environment.d/01-native-wayland.conf)
+
 	set -x XDG_SESSION_TYPE wayland
-	set -x XDG_CURRENT_DESKTOP kde
+
 	exec sway
 end
